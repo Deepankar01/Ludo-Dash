a2eec53243116bdd5b116d2bb96ef1e5e396d75c
Deepankar Agrawal <deepankar_agrawal@external.mckinsey.com>
2020-04-26 12:03:43 +0530

    added getter

--- lib/Physics/Physics.dart
+++ lib/Physics/Physics.dart
@@ -9,11 +9,11 @@ class Physics {
   static int row = 15;
   static int col = 15;
   final List<Player> players;
-  List<List<Token>> board;
+  List<List<Token>> _board;
 
   Physics(this.players) {
     assert(players.length <= 4);
-    board = _resetBoard();
+    _board = _resetBoard();
   }
 
   List<List<Token>> _resetBoard() {
@@ -26,6 +26,8 @@ class Physics {
   Token _getPlayerHomeLocation(int rowIndex, int colIndex) {
     return Token(Vector2.zero(), this.players[0]);
   }
+
+  List<List<Token>> get board => _board;
 
   //mutator functions
   void _changePlayerTurn(Player currentPlayer) {}
